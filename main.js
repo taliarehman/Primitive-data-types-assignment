f9bd703022aa739a6d1c7d7b97d61fa4030be229
talia rehman <159155651+taliarehman@users.noreply.github.com>
2024-03-05 01:35:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// PRIMITIVE DATA TYPES:
+// STRING:
+var myName = "Taliaehman";
+console.log(myName);
+//  NUMBER:
+var myAge = 18;
+console.log(myAge);
+// BOOLEAN:
+var statement = true;
+console.log('Are you from Pakistan', statement == true);
+// NULL:
+var x = null;
+console.log(null);
+// UNDEFINED:
+var y = undefined;
+console.log(y);
---
+++ main.ts
@@ -0,0 +1,21 @@
+// PRIMITIVE DATA TYPES:
+
+// STRING:
+let myName: string = "Taliaehman";
+console.log(myName);
+
+//  NUMBER:
+let myAge: number = 18;
+console.log(myAge);
+
+// BOOLEAN:
+let statement: boolean = true;
+console.log('Are you from Pakistan',statement == true);
+
+// NULL:
+let x: null = null;
+console.log(null);
+
+// UNDEFINED:
+let y: undefined = undefined
+console.log(y);
